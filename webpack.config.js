0673cdcba1d3588d17e2caebf2011632ba1a6d89
Ninjaphil24 <philipmodino@gmail.com>
2022-03-22 18:37:06 +0100

    scss part 2+correction

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,15 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 let mode = "development";
 
-if(process.env.NODE_ENV === "production")mode="production";
+if (process.env.NODE_ENV === "production") mode = "production";
 
 module.exports = {
     mode: mode,
-    
+
     module: {
-        rules: [
-            {
-                test: /\.s?css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader","sass-loader"],
+        rules: [{
+                test: /\.(s[ac]|c)ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
             },
             {
                 test: /\.js$/,
