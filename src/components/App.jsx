d7dd7fbaf329c3bb28589729c3f6cc085527d6d9
Ninjaphil24 <philipmodino@gmail.com>
2022-03-23 16:25:04 +0100

    add image support to webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,19 @@
 import "../styles/index.scss";
 import Recipes from "./Recipes";
+import concert from "../images/FotoKyttaro2.jpg";
 
 const App = () => {
     return (
         <>
         <section className="hero"></section>
         <main>
-            <section>
-                <h1>Oh Hi, React</h1>
-            </section>
+                <section>
+                    <h1>Oh Hi, React</h1>
+                </section>
+                <img src={concert} alt="concert" width="250"/>
+            <Recipes />
         </main>
 
-        <Recipes />
         </>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,23 @@ if (process.env.NODE_ENV === "production") mode = "production";
 module.exports = {
     mode: mode,
 
+    output: {
+        assetModuleFilename: "images/[hash][ext][query]"
+    },
+
     module: {
-        rules: [{
+        rules: [
+            {
+               test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset",
+                // Allows you to determine size of images to be turned to inline code.  Default is 8kb
+                // parser: { 
+                //     dataUrlCondition: {
+                //         maxSize: 30 * 1024,
+                //     },
+                // },
+            },
+            {
                 test: /\.(s[ac]|c)ss$/i,
                 use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
             },
